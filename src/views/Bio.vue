98cac067d9135703d2ecb0799c1245881e4668be
Jack Tsai <jtsai@nexify.com.hk>
2025-02-26 18:01:17 +0800

    add bio info

--- src/views/Bio.vue
+++ src/views/Bio.vue
@@ -1,4 +1,43 @@
 <template>
-  bio
+  <div class="container">
+    <div class="content">
+      <H1>👋 Hey, I’m Jack</H1>
+      <H2>👨‍💻 A passionate backend developer from Taiwan</H2>
+      <p>
+        With a deep passion for backend development, I specialize in creating robust and scalable web applications. My expertise lies in modern web technologies, and I am currently contributing as a backend developer at XXX. I am eager to collaborate on innovative projects and continuously expand my skill set.
+      </p>
+      <H2>📚 My Skills</H2>
+      <ul>
+        <li>C#</li>
+        <li>Vue.js</li>
+        <li>Node.js</li>
+        <li>Express.js</li>
+        <li>MSSQL</li>
+        <li>Git</li>
+        <li>RESTful APIs</li>
+      </ul>
+      <H2>📫 How to reach me</H2>
+      <ul>
+        <li>Email: yentsai777@gmail.com</li>
+        <li>LinkedIn: <a href="https://www.linkedin.com/in/jack-tsai-1033721a5/">LinkedIn</a></li>
+        <li>GitHub: <a href="https://github.com/yanzzzzzzzzz">https://github.com/yanzzzzzzzzz</a></li>
+      </ul>
+    </div>
+  </div>
 </template>
+
 <script setup lang="ts"></script>
+
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  padding: 1rem;
+}
+
+.content {
+  max-width: 800px;
+}
+</style>
