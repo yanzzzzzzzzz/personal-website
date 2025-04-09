3be1570f23640399fee8da28a234c45dc9262c52
Jack Tsai <jtsai@nexify.com.hk>
2025-04-09 10:52:04 +0800

    add project desc

--- src/views/Project.vue
+++ src/views/Project.vue
@@ -8,7 +8,7 @@
         class="bg-white shadow-md rounded-lg overflow-hidden cursor-pointer"
         @click="goToDetail(project.id)"
       >
-        <img :src="project.image" alt="Project Image" class="w-full h-48 object-cover">
+        <img :src="project.image" alt="Project Image" class="w-full h-48 object-contain">
         <div class="p-4">
           <h2 class="text-xl font-semibold text-gray-800">{{ project.name }}</h2>
           <p class="text-gray-600 mt-2">{{ project.shortDescription }}</p>
@@ -51,10 +51,37 @@ const projects = ref<Project[]>([
     id: 2,
     name: 'URL Shortener',
     shortDescription: 'An URL shortener',
-    skills: ['React', 'CSS', 'TypeScript'],
-    image: 'images/project-two-image.jpg',
+    skills: ['Vue', 'TypeScript'],
+    image: 'images/short-url.png',
   },
-  // Add more projects as needed
+  {
+    id: 3,
+    name: 'Goose Farm Egg Production Analysis System',
+    shortDescription: 'A system for analyzing egg production in a goose farm',
+    skills: ['C#', 'Python', 'YOLO'],
+    image: 'images/goose-farm.png',
+  },
+  {
+    id: 4,
+    name: 'Panel Detection Algorithm Development',
+    shortDescription: 'A panel detection algorithm development project',
+    skills: ['MATLAB', 'image classification', 'Template Matching'],
+    image: 'images/panel-detection.jpg',
+  },
+  {
+    id: 5,
+    name: 'Hand Rehabilitation Game',
+    shortDescription: 'A hand rehabilitation game',
+    skills: ['Unity', 'C#'],
+    image: 'images/hand-rehabilitation-game.png',
+  },
+  {
+    id: 6,
+    name: 'Indoor Design Project Management System',
+    shortDescription: 'An indoor design project management system',
+    skills: ['Vue', 'C#', 'MSSQL'],
+    image: 'images/indoor-design-project-management-system.png',
+  }
 ]);
 
 const router = useRouter();
